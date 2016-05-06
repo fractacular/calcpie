96435432528c8cb6b0d1594980e2e07dc7aaafe1
Scott <scott.e.sutherland@gmail.com>
2016-05-06 15:51:02 -0400

    Update pie.js

--- pie.js
+++ pie.js
@@ -2,7 +2,7 @@
 /*
 MIT License
 
-Copyright (c) [year] [fullname]
+Copyright (c) 2013 Scott Sutherland
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,6 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
 // object fix
 if(typeof Object.create !== 'function'){
 	Object.clreate = function(obj){
